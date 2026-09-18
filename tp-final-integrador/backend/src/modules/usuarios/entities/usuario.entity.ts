ce57890c414bd7e9b4b85e7f94ebd0f966741e92
Horacio Belardita <horaciobelardita@gmail.com>
2026-09-18 00:45:20 -0300

    feat(usuarios): crear entidad TypeORM Usuario con proteccion de clave y enums

---
+++ tp-final-integrador/backend/src/modules/usuarios/entities/usuario.entity.ts
@@ -0,0 +1,48 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  type Relation,
+} from 'typeorm';
+import { EstadoUsuario, RolUsuario } from '../enums/index.js';
+import { Medico } from '../../medicos/entities/medico.entity.js';
+
+@Entity('usuarios')
+export class Usuario {
+  @PrimaryGeneratedColumn({ type: 'int' })
+  id: number;
+
+  @Column({ type: 'varchar', length: 20, unique: true })
+  documento: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  apellidos: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  nombres: string;
+
+  @Column({ type: 'varchar', length: 150 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255, select: false })
+  clave: string;
+
+  @Column({
+    type: 'enum',
+    enum: EstadoUsuario,
+    enumName: 'estados_usuarios',
+    default: EstadoUsuario.ACTIVO,
+  })
+  estado: EstadoUsuario;
+
+  @Column({
+    type: 'enum',
+    enum: RolUsuario,
+    enumName: 'roles_usuarios',
+  })
+  rol: RolUsuario;
+
+  @OneToOne(() => Medico, (medico) => medico.usuario)
+  medico?: Relation<Medico>;
+}
